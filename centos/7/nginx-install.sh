962389af52106c15134adb990637e4ac055eb794
Boris Ivanov <bivanov@pharmanet.ru>
2015-08-27 14:55:31 +0300

    dfg

--- centos/7/nginx-install.sh
+++ centos/7/nginx-install.sh
@@ -5,6 +5,7 @@ sudo systemctl enable nginx
 
 sudo /usr/bin/firewall-cmd --zone=public --add-service=http # Opens port now
 sudo /usr/bin/firewall-cmd --permanent --zone=public --add-service=http # Makes change persistent
-sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo systemctl restart firewalld.service
+#sudo firewall-cmd --permanent --zone=public --add-service=https
 
-sudo usermod -a -G *user* nginx
+#sudo usermod -a -G *user* nginx
