2baed35b52012991d5eb2d40081fb0ec49d79f85
Boris Ivanov <bivanov@pharmanet.ru>
2015-09-04 14:57:03 +0300

    backup

--- centos/7/postgresql-restore-from-ya-disk.sh
+++ centos/7/postgresql-restore-from-ya-disk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install YaDiskClient
 wget https://raw.githubusercontent.com/BorisIvanov/vps-manage/master/centos/7/ya_download.py
-python ya_download.py
-unzip $YA_DST.zip
-pg_restore -U postgres -c -d $YA_DB_NAME $YA_DST.dump
+python3.4 ya_download.py
+unzip $YA_FILE_NAME.zip
+pg_restore -U postgres -c -d $YA_DB_NAME tmp/$YA_FILE_NAME.dump
