f5d9729707f29c039060a49e91c4fe6c1db8749d
Boris Ivanov <ibv_mail@mail.ru>
2015-09-06 14:17:58 +0300

    rm before

--- centos/7/ya_download.py
+++ centos/7/ya_download.py
@@ -1,5 +1,13 @@
 import os
+import shutil
 from YaDiskClient import YaDisk
 
+try:
+	os.remove(os.getenv('YA_DST'))
+except OSError:
+    pass	
+
+shutil.rmtree('tmp', ignore_errors=True, onerror=None)	
+	
 disk = YaDisk(os.getenv('YA_LOGIN'), os.getenv('YA_PASS'))
 disk.download(os.getenv('YA_SRC'), os.getenv('YA_DST'))
